62b01ea68300294f4e661db4224e49ee493799a6
Marinos Poiitis <marinospoet@gmail.com>
2020-03-25 00:03:27 +0200

    Add dataset merging

--- get_data.py
+++ get_data.py
@@ -2,7 +2,7 @@ import urllib.request
 from datetime import datetime, timedelta, time
 import datetime as dt
 import pandas as pd
-import pickle
+import numpy as np
 
 def get_ecdc_data():
     datetimeToday = datetime.today()
@@ -35,5 +35,29 @@ def get_ecdc_data():
                 print ('no data yet')
 
 
+def merge_data():
+    """
+    Merges 3 datasets
+    """
+    ecdc = pd.read_excel('data/ecdcdata.csv', converters={'GeoId':str})
+    oecd = pd.read_csv('data/oecd_data.csv')
+    world_bank = pd.read_csv('data/world_bank_data.csv', encoding='cp1252')
+
+    # convert countries to lowercase
+    oecd['Countries and territories'] = [c.lower() for c in oecd['Countries and territories']]
+    ecdc['Countries and territories'] = [c.lower() for c in ecdc['Countries and territories']]
+    world_bank['Countries and territories'] = [c.lower() for c in world_bank['Countries and territories']]
+
+    summaries = ecdc.groupby(['Countries and territories']).agg({'Cases':  np.sum, 'Deaths': np.sum}).reset_index()
+    merged = pd.merge(summaries, oecd, on="Countries and territories", how='left') # keep all countries in ecdc data
+
+    # remove whitespaces
+    merged['Countries and territories'] = [c.strip() for c in merged['Countries and territories']]
+    world_bank['Countries and territories'] = [c.lower().strip() for c in world_bank['Countries and territories']]
+
+    merged = pd.merge(merged, world_bank, on="Countries and territories", how='left') # keep all countries in ecdc data
+    merged.to_csv('data/merged_data.csv', index=False)
+
 if __name__ == "__main__":
     get_ecdc_data()
+    merge_data()
